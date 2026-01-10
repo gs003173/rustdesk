084153680e357442c79568e4a339efe38ddd3c9c
gs003173 <37007985+gs003173@users.noreply.github.com>
2026-01-10 13:59:52 +0800

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1,4 +1,4 @@
-import 'dart:async';
+ID/Relay Serverimport 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -1634,12 +1634,6 @@ class _NetworkState extends State<_Network> with AutomaticKeepAliveClientMixin {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if (!hideServer)
-                listTile(
-                  icon: Icons.dns_outlined,
-                  title: 'ID/Relay Server',
-                  onTap: () => showServerSettings(gFFI.dialogManager, setState),
-                ),
               if (!hideProxy && !hideServer) divider,
               if (!hideProxy)
                 listTile(
